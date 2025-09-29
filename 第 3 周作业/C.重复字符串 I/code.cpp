9318ad9c5a82639bdc74180597ab622dd2d9f1f3
CloudingYu <24300240045@m.fudan.edu.cn>
2025-09-29 08:18:29 +0800

    优化代码，合并变量声明，修复循环条件，简化逻辑

--- 第 3 周作业/C.重复字符串 I/code.cpp
+++ 第 3 周作业/C.重复字符串 I/code.cpp
@@ -1,44 +1,41 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include<algorithm>
 using namespace std;
 
-int n;
-int Next[5050];
+int n, Next[5050], maxmatch = 0;
 string s, p, t;
 
 int main()
 {
     cin >> n >> s;
-
-    int max_match = 0;
-
     for (int len = 1; len < n; len++)
     {
         p = s.substr(len);
         t = s.substr(0, len);
 
         Next[0] = -1;
-        for (int i = 0, k = -1; i < n - len - 1;)
-            if (k == -1 || p[i] == p[k])
+        for (int i = 0, k = -1; i < n - len-1; i++)
+            if (k == -1 || p[k] == p[i])
                 Next[++i] = ++k;
             else
                 k = Next[k];
 
         int i = 0, j = 0;
-        while (i < len && j < n - len)
+        while (i < len && j < n-len)
         {
             if (t[i] == p[j])
                 i++, j++;
             else if (Next[j] != -1)
-                j = Next[j];
-            else
+                j=Next[j];
+            else 
                 j = 0, i++;
-            max_match = max(max_match, j);
-        } 
+
+            maxmatch = max(maxmatch, j);
+        }
     }
 
-    cout << max_match << endl;
-
+    cout << maxmatch << endl;
+    
     return 0;
 }
