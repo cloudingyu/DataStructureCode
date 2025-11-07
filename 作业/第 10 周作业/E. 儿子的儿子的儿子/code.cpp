cec8f336d33b67b56d6f56e294ea2f01f4712460
CloudingYu <24300240045@m.fudan.edu.cn>
2025-11-07 22:48:15 +0800

    完成第十周作业

--- 作业/第 10 周作业/E. 儿子的儿子的儿子/code.cpp
+++ 作业/第 10 周作业/E. 儿子的儿子的儿子/code.cpp
@@ -1,77 +1,47 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
 const int MAXN = 5e6 + 5;
 
-int n, k;
-int head[MAXN], to[MAXN], nxt[MAXN];
-int ans[MAXN];
-
-int stackNode[MAXN], stackEdge[MAXN];
-unsigned char stackEntered[MAXN];
-
-int edgeCnt = 0;
-
-inline void add_edge(int u, int v)
+struct Edge
 {
-    to[++edgeCnt] = v;
-    nxt[edgeCnt] = head[u];
-    head[u] = edgeCnt;
+    int to, nxt;
+} edges[MAXN];
+
+int n, k, fa, edgeCnt = 0;
+int guide[MAXN], ans[MAXN], path[MAXN];
+
+void addEdge(int u, int v)
+{
+    edges[++edgeCnt].to = v;
+    edges[edgeCnt].nxt = guide[u];
+    guide[u] = edgeCnt;
+}
+
+void dfs(int u, int depth)
+{
+    path[depth] = u;
+
+    if (depth >= k)
+        ++ans[path[depth - k]];
+
+    for (int e = guide[u]; e != 0; e = edges[e].nxt)
+        dfs(edges[e].to, depth + 1);
 }
 
 int main()
 {
-
-    cin >> n >> k;
-    fill(head, head + n + 1, -1);
+    scanf("%d%d", &n, &k);
 
     for (int i = 2; i <= n; ++i)
     {
-        int fa;
-        cin >> fa;
-        add_edge(fa, i);
+        scanf("%d", &fa);
+        addEdge(fa, i);
     }
 
-    int sp = 0;
-    stackNode[0] = 1;
-    stackEdge[0] = head[1];
-    stackEntered[0] = 0;
-
-    while (sp >= 0)
-    {
-        int u = stackNode[sp];
-
-        if (!stackEntered[sp])
-        {
-            stackEntered[sp] = 1;
-            if (sp >= k)
-            {
-                int ancestor = stackNode[sp - k];
-                ++ans[ancestor];
-            }
-        }
-
-        int e = stackEdge[sp];
-        if (e != -1)
-        {
-            stackEdge[sp] = nxt[e];
-            int v = to[e];
-            stackNode[++sp] = v;
-            stackEdge[sp] = head[v];
-            stackEntered[sp] = 0;
-        }
-        else
-        {
-            --sp;
-        }
-    }
+    dfs(1, 0);
 
     for (int i = 1; i <= n; ++i)
-    {
-        if (i > 1)
-            cout << ' ';
-        cout << ans[i];
-    }
-    cout << '\n';
+        printf("%d ", ans[i]);
+
     return 0;
 }
